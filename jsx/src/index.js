832c22e2d7becca6b450a1bfbca135ef2a8b801c
Taylor Savage <taylorsavagedesign@gmail.com>
2019-01-22 13:05:50 -0600

    Notes on JSX and style syntax

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -8,9 +8,21 @@ import ReactDOM from 'react-dom';
 // Create a react component
 // Component is function or class to produce HTML to show to user w/JSX and handle user feedback w/event listeners
 
+// Adding custom styling to element in JSX requires {{ }} around style definition, with camelCase and single quote around value and commma between style definitions
+
 const App = function() {
-	return <div>Hi there!</div>;
+	return (
+		<div>
+			<label class="label" for="name">Enter name:</label>
+			<input id="name" type="text" />
+			<button style={{ backgroundColor: 'blue', color: 'white'}}>Submit</button>
+		</div>
+	);
 };
+
+// Use babeljs.io to try out conversion from React/JSX to ES5
+
+// JSX allows us to write cleaner code that then gets converted to function calls of .createElement()
 
 // Take component and display on screen
 
