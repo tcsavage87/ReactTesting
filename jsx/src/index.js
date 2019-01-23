8605a5939a2205cb12d7ab1f33fb2977f9cfe45e
Taylor Savage <taylorsavagedesign@gmail.com>
2019-01-22 23:42:53 -0600

    Passing variables, functions and objects into JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,29 +1,43 @@
-// Import the React and ReactDOM libraries
+// Step 1. Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import statement refers to ES2015 modules
 // require statement refers to CommonJS modules
 
-// Create a react component
-// Component is function or class to produce HTML to show to user w/JSX and handle user feedback w/event listeners
+// Step 2. Create a react component
+	// Component is function or class to produce HTML to show to user w/JSX and handle user feedback w/event listeners
 
-// Adding custom styling to element in JSX requires {{ }} around style definition, with camelCase and single quote around value and commma between style definitions
+// Adding custom styling to element in JSX requires {{ }} around style definition as an object, with camelCase and single quote around value and commma between style definitions
+
+// Use " " for a string, but for any non-JSX property use ' ' such as style values
+
+// Use className for class, htmlFor in place of for, and use { } within JSX to reference Javascript variable or function call
+
+	// Cannot pass entire JS object as a reference within JSX for text, must refer to specific property (key)
 
 const App = function() {
+	const buttonText = 'Click me!';
+	const labelText = 'Enter name:';
+
 	return (
 		<div>
-			<label class="label" for="name">Enter name:</label>
+			<label className="label" htmlFor="name">
+			{labelText}
+			</label>
 			<input id="name" type="text" />
-			<button style={{ backgroundColor: 'blue', color: 'white'}}>Submit</button>
+			<button style={{ backgroundColor: 'blue', color: 'white'}}>
+			{buttonText}
+			</button>
 		</div>
 	);
 };
 
-// Use babeljs.io to try out conversion from React/JSX to ES5
+	// Use babeljs.io to try out conversion from React/JSX to ES5
 
-// JSX allows us to write cleaner code that then gets converted to function calls of .createElement()
+	// JSX allows us to write cleaner code that then gets converted to function calls of .createElement()
 
-// Take component and display on screen
+// Step 3. Take component and display on screen
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
