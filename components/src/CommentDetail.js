139bb795a4fa1d422a2ae10cfb1f01694e81e31a
Taylor Savage <taylorsavagedesign@gmail.com>
2019-01-24 10:50:32 -0600

    Comment layout with props and reusable component

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const CommentDetail = props => {
+	return (
+		<div className="comment">
+			<a href="/" className="avatar">
+				<img src={props.avatar} alt="avatar" />
+			</a>
+			<div className="content">
+				<a href="/" className="author">{props.author}</a>
+				<div className="metadata">
+					<span className="date">{props.time}</span>
+				</div>
+				<div className="text">{props.textComment}</div>
+			</div>
+		</div>
+	);
+}
+
+// Props - system for passing data from parent to child/nested component
+	// Goal to communicate data to customize child component
+	// Pass props argument into function, refer to props.key to pass information
+
+
+// Use export to make component available to rest of project
+export default CommentDetail;
--- components/src/index.js
+++ components/src/index.js
@@ -1,59 +1,43 @@
+// to use code from another file/dependency/project must use import
 import React from 'react';
 import ReactDOM from 'react-dom';
-// to use code from another file/dependency/project must use import
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
+// to make resuable configurable component
+	// identify jsx to be duplicated
+	// create new file to house component named after component
+	// create new component in file, paste jsx inside
+	// make configurable by using 'props'
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img src={faker.image.avatar()} alt="avatar" />
-				</a>
-				<div className="content">
-					<a href="/" className="author">
-						Sam
-					</a>
-					<div className="metadata">
-						<span className="date">Today at 6:00pm</span>
-					</div>
-					<div className="text">Nice blog post
-					</div>
-				</div>
-			</div>
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img src={faker.image.avatar()} alt="avatar" />
-				</a>
-				<div className="content">
-					<a href="/" className="author">
-						Jill
-					</a>
-					<div className="metadata">
-						<span className="date">Today at 6:02pm</span>
-					</div>
-					<div className="text">Great information!
-					</div>
-				</div>
-			</div>
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img src={faker.image.avatar()} alt="avatar" />
-				</a>
-				<div className="content">
-					<a href="/" className="author">
-						Greg
-					</a>
-					<div className="metadata">
-						<span className="date">Today at 6:05pm</span>
-					</div>
-					<div className="text">Thanks for posting
-					</div>
-				</div>
-			</div>
+			<CommentDetail 
+				avatar={faker.image.avatar()} 
+				author="Sam" 
+				time="Yesterday at 6:00pm" 
+				textComment="Nice blog post!" 
+			/>
+			<CommentDetail 
+				avatar={faker.image.avatar()} 
+				author="Becky" 
+				time="Today at 6:15pm" 
+				textComment="Very helpful" 
+			/>
+			<CommentDetail 
+				avatar={faker.image.avatar()} 
+				author="Tina" 
+				time="Today at 7:03pm" 
+				textComment="Thanks for posting" 
+			/>
 		</div>
 	);
 };
+
+// Define props within parent component by passing attribute into JSX tag
+
+// To show component within another, use JSX tag
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
