0f9cf6cfcd6796f68fbb16545e1eb409745b9ca0
Taylor Savage <taylorsavagedesign@gmail.com>
2019-01-28 17:41:12 -0600

    State introduction, error handling

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -9,30 +9,55 @@
 	// Within functional component, unable to wait to render JSX until geolocation returns
 	// Class-based component (with state) allows this functionality by telling component to re-render JSX with returned geolocation info
 
-
 // Class-based components
 	// 1. Must be JS Class
 	// 2. Must extend (subclass) React.Component
 	// 3. Must define a 'render' method that returns some JSX
 		// Other methods included within React.Component
 
-// Show different text/icons based on props
+// State
+	// 1. Only usable with class-based components (unless w/ hooks)
+	// 2. State is a JS Object that contains data relevant to component
+	// 3. Updating state on a component causes the component to instantly render
+	// 4. State must be initialized upon component creation
+	// 5. State can only be updated using setState()
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-	render() {
+	// First function called on instance of class is constructor to initialize state
+	constructor(props) {
+		// super passes props to refer to parent class' constructor fxn
+		super(props); 
+
+		// initialization of state object, only time direct assignment
+		this.state = { lat: null, hemisphere: '', errorMessage: '' }; 
+		
 		window.navigator.geolocation.getCurrentPosition(
 			position => {
-				let hemisphere;
-				position.coords.latitude > 0 ? hemisphere = 'North' : hemisphere = 'South';
-				console.log(hemisphere);
+				// call setState() to update state object when success, pass object with update to object
+				this.setState({	lat: position.coords.latitude });
 			},
-			err => console.log(err)
+			err => {
+				this.setState({ errorMessage: err.message });
+			}
 		);
-		return <div>Latitude</div>;
+	}
+	// React requires that we define render
+	render() {	
+		// Conditional rendering - returning different JSX depending on state of component
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <div>Latitude: {this.state.lat}</div>;
+		}
+	
+		return <div>Loading...</div>;
 	}
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
