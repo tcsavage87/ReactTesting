c127af4461b31a8f3d39fd1658a378b55cba8dc6
Taylor Savage <taylorsavagedesign@gmail.com>
2019-02-05 14:30:07 -0600

    ImageCard component and ImageList stylesheet

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,23 +1,18 @@
+import './ImageList.css';
+import ImageCard from './ImageCard';
 import React from 'react';
 
 // when returning multiple elements, by iterating over a list, important to include key property on the root element that matches key on list data
 
 const ImageList = (props) => {
-	console.log(props.images);
-	const images = props.images.map(({ description, id, urls}) => {
-		return (
-			<img 
-				alt={description}
-				key={id}
-				src={urls.regular} 
-			/>
-		);
+	const images = props.images.map(image => {
+		return <ImageCard image={image} key={image.id} />;
 	});
+	console.log(images);
 
 	return (
-		<div>{images}</div>
+		<div className="image-list">{images}</div>
 	);
 };
-
 
 export default ImageList;
