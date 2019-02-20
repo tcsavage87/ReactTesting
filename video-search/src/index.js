ae87d18c30209e901f0be93987af17f915f0c760
Taylor Savage <taylorsavagedesign@gmail.com>
2019-02-19 23:01:27 -0600

    Git ignore updates

--- video-search/src/index.js
+++ video-search/src/index.js
@@ -1,5 +1,5 @@
-import App from './components/App.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.js';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
