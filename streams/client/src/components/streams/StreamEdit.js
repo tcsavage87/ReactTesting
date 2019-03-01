b0c010f46c1362a5443cb05aa55aba901714428b
Taylor Savage <taylorsavagedesign@gmail.com>
2019-03-01 15:46:02 -0600

    StreamEdit component data loading

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
-	return <div>StreamEdit</div>;
+// React-router-dom automatically passes props down to Routes
+class StreamEdit extends React.Component {
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+
+	render() {
+		console.log(this.props);
+		if (!this.props.stream) {
+			return <div>Loading...</div>;
+		}
+		return <div>{this.props.stream.title}</div>;
+	}
 };
 
-export default StreamEdit;
+// use ownProps argument to pass stream id from URL and access state object of stream with that key
+const mapStateToProps = (state, ownProps) => {
+	return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(
+	mapStateToProps, 
+	{ fetchStream }
+)(StreamEdit);
