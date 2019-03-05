febcff159756194d79ca14f0ae45eebae3a6b6ed
Taylor Savage <taylorsavagedesign@gmail.com>
2019-03-05 11:13:31 -0600

    Display content on StreamShow page

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // To use history object, must use plain Router
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -15,7 +15,7 @@ import history from '../history';
 // Use Link Component in place of anchor tags
 
 // Exact keyword is used to ensure Route is only displayed with exact URL matching path
-
+	// Switch will only show one route for any given path, preventing multiple Routes being shown that match the path
 // To show on all pages, define content outside of BrowserRouter
 const App = () => {
 	return (
@@ -23,11 +23,13 @@ const App = () => {
 			<Router history={history}>
 				<div>
 					<Header />
-					<Route path="/" exact component={StreamList} />
-					<Route path="/streams/new" exact render={() => <StreamCreate />} />
-					<Route path="/streams/edit/:id" exact component={StreamEdit} />
-					<Route path="/streams/delete/:id" exact component={StreamDelete} />
-					<Route path="/streams/show" exact component={StreamShow} />
+					<Switch>
+						<Route path="/" exact component={StreamList} />
+						<Route path="/streams/new" exact render={() => <StreamCreate />} />
+						<Route path="/streams/edit/:id" exact component={StreamEdit} />
+						<Route path="/streams/delete/:id" exact component={StreamDelete} />
+						<Route path="/streams/:id" exact component={StreamShow} />
+					</Switch>
 				</div>
 			</Router>
 		</div>
--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -29,13 +29,15 @@ class StreamList extends React.Component {
 			return (
 				<div className="item" key={stream.id}>
 					{this.renderAdmin(stream)}
-					<i className="large middle aligned icon camera" />
-					<div className="content">
-						{stream.title}
-						<div className="description">
-							{stream.description}
+						<i className="large middle aligned icon camera" />
+						<div className="content">
+							<Link to={`/streams/${stream.id}`} className="header">
+								{stream.title}
+							</Link>
+							<div className="description">
+								{stream.description}
+							</div>
 						</div>
-					</div>
 				</div>
 			);
 		});
