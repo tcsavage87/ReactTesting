28c1912287ae18c434b5a8dbc2203e6cbb1fac7a
Taylor Savage <taylorsavagedesign@gmail.com>
2019-03-05 22:37:35 -0600

    Provider and Consumer hookup, color button implementation

--- translation/src/components/App.js
+++ translation/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 // Context API
 	// Use context object to supply data to nested children
@@ -8,12 +10,20 @@ import UserCreate from './UserCreate';
 		// 2) Create Provider Object within Parent Component
 	// 2 ways to get information out
 		// 1) Reference this.context 
-		// 2) create Consumer Component
+		// 2) create Consumer Component - created automatically upon context object creation
+
+// Provider creates a unique Context pipe of data to share with nested components on each new instance within App
 
 class App extends React.Component {
-	state = { language: 'english' }
+	state = { language: 'english', color: 'red' }
+
+	onColorChange = (color) => {
+		console.log(color);
+		this.setState({ color });
+	};
 
 	onLanguageChange = (language) => {
+		console.log(language);
 		this.setState({ language });
 	};
 
@@ -22,10 +32,35 @@ class App extends React.Component {
 	      	<div className="ui container">
 	        	<div>
 	        		Select a language:
-	        		<i className="flag us" onClick={() => this.onLanguageChange('english')} />
-	        		<i className="flag nl" onClick={() => this.onLanguageChange('dutch')}/>
+	        		<i 
+	        			className="flag us" 
+	        			onClick={() => this.onLanguageChange('english')} 
+	        			style={{cursor: 'pointer'}}
+	        		/>
+	        		<i 
+	        			className="flag nl" 
+	        			onClick={() => this.onLanguageChange('dutch')}
+	        			style={{cursor: 'pointer'}}
+	        		/>
 	        	</div>
-	        	<UserCreate language={this.state.language} />
+	        	<div>
+	        		Select a color:
+	        		<i 
+	        			className="circle icon red" 
+	        			onClick={() => this.onColorChange('red')} 
+	        			style={{cursor: 'pointer'}}
+					/>
+	        		<i 
+	        			className="circle icon blue" 
+	        			onClick={() => this.onColorChange('blue')}
+	        			style={{cursor: 'pointer'}}
+					 />
+	        	</div>
+	        	<ColorContext.Provider value={this.state.color}>
+		        	<LanguageContext.Provider value={this.state.language}>
+		        		<UserCreate />
+		      		</LanguageContext.Provider>
+		      	</ColorContext.Provider>
 	      	</div>
 	    );
 	}
--- translation/src/components/Button.js
+++ translation/src/components/Button.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
-	static contextType = LanguageContext;
 
-	render() {
-		const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+	renderButton(color) {
 		return (
-			<button className="ui button primary">
-				{text}
+			<button className={`ui button ${color}`}>
+				<LanguageContext.Consumer>
+					{language => language === 'english' ? 'Submit' : 'Voorleggen'}
+				</LanguageContext.Consumer>
 			</button>
-		);	
+		);
 	}
 	
+	// no contextType required with Consumer
+	// Place consumer where content should be rendered and include function as child of component 
+	render() {
+		return (
+			<ColorContext.Consumer>	
+				{color => this.renderButton(color)}
+			</ColorContext.Consumer>
+		);	
+	}
 };
 
 export default Button;
