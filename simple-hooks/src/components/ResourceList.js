a5aa99f7d3bc50ba753edef4a9b8b79605452f2a
Taylor Savage <taylorsavagedesign@gmail.com>
2019-03-06 22:35:18 -0600

    Refactor ResourceList to function with useState

--- simple-hooks/src/components/ResourceList.js
+++ simple-hooks/src/components/ResourceList.js
@@ -1,25 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-	state = { resources: [] };
+const ResourceList = () => {
+	// Initialization of state
+	const [resources, setResources] = useState([]);
 
-	async componentDidMount() {
+	const fetchResource = async () => {
 		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-		this.setState({ resources: response.data });
+		setResources(response.data);
 	}
 
-	// use prevProps to check on previous props and only run if different from currnent value
-	async componentDidUpdate(prevProps) {
-		if (prevProps.resource !== this.props.resource) {
-			const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-			this.setState({ resources: response.data });
-		}
-	}
-
-	render() {
-		return <div>{this.state.resources.length}</div>;
-	}
+	return <div>{resources.length}</div>;
 }
 
 export default ResourceList;
